1159d8b86b6d11dfeaf4f5c2b66bba3a89029497
as3091 <as3091@gmail.com>
2025-04-13 14:16:40 +0000

    $(date +"%Y%m%d_%H%M%S") streamlit pkl stuff

--- ML/Assign_2/NER/streamlit_app.py
+++ ML/Assign_2/NER/streamlit_app.py
@@ -73,7 +73,7 @@ input_string = st.text_area("Enter your string :",
 
 p = Popen("pwd", stdout=PIPE, stderr=PIPE,shell =True)
 out, err = p.communicate()
-pwd = out.decode()
+pwd = out.decode().strip()
 st.write(f"pwd {pwd}", out.decode())
 
 cmd = f"ls -ltrh {pwd}/ML/Assign_2/NER/*"
